9307d753af710775f0ff886d90e1357232581507
Jean Kang <jeankang.dev@gmail.com>
2021-09-24 04:18:47 -0400

    create functions to fetch and manage toDoItems

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -39,8 +39,10 @@ function showCompleted() {
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems(todo1, todo2) {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  addTodo(todo1);
+  addTodo(todo2);
 }
 
 // NOTE: 유틸 함수
@@ -48,6 +50,9 @@ function log() {
   console.log(todoItems);
 }
 
+let todo1 = { id: 4, title: '타입', done: false };
+let todo2 = { id: 5, title: '타입', done: false };
+
 todoItems = fetchTodoItems();
-addTwoTodoItems();
+addTwoTodoItems(todo1, todo2);
 log();
