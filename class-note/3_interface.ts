82d5195fb0265b9af80dfc3911f8747eef52e86f
Jean Kang <jeankang.dev@gmail.com>
2021-09-25 10:12:28 -0400

    declare interface and use it for parameters and functions

---
+++ class-note/3_interface.ts
@@ -0,0 +1,19 @@
+interface User {
+  hobby: string;
+}
+
+// utilize interface on parameters
+let jean: User = {
+  hobby: 'swimming'
+}
+
+// utilize interface for functions
+function getUser(user: User) {
+  console.log(user);
+}
+
+const capt = {
+  hobby: 'fighting'
+}
+
+getUser(capt);
