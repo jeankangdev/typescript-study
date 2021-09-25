f43652bc2c1b01cae4877f2810acbfe4ee7518c3
Jean Kang <jeankang.dev@gmail.com>
2021-09-25 11:30:38 -0400

    extend interface

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -27,3 +27,50 @@ let sum: SumFunction;
 sum = function(a: number, b: number): number {
   return a + b;
 }
+
+// indexing
+interface StringArray {
+  [index: number]: string;
+}
+
+let arr: ['a', 'b', 'c'];
+arr[0]; // 'a'
+
+// dictionary pattern
+interface StringRegexDictionary {
+  [key: string]: RegExp;
+}
+
+let obj = {
+  sth: /abc/,
+  cssFile: /\.css$/,
+  jsFile: /\.js$/
+}
+
+obj['cssFile'] = /\.css$/;
+
+Object.keys(obj).forEach(function(vaue) {
+  
+});
+
+// extending interface
+interface Person {
+  name: string;
+  age: number;
+}
+
+// interface Developer {
+//   name: string;
+//   age: number;
+//   language: string;
+// }
+
+interface Developer extends Person {
+  language: string;
+}
+
+let dev: Developer = {
+  name: 'Jean',
+  age: 0,
+  language: 'multi'
+}
